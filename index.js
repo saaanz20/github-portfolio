1052c9a54d5252ce3159aae6aab0bfeccbea9d6d
saaanz20 <sanzpapa5007@gmail.com>
2026-02-19 12:49:30 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const message = document.getElementById("project-message");
+    message.textContent = "https://saaanz20.github.io/github-portfolio/";
+
+    window.open("calculator.html", "_blank");
+}
